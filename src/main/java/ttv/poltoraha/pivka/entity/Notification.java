6536a736a6716943a90b5f8450d22a6632a5493d
Ilya Vehov <vehov.3@gmail.com>
2025-05-16 01:52:13 +0400

    chore: add new notification table

---
+++ src/main/java/ttv/poltoraha/pivka/entity/Notification.java
@@ -0,0 +1,21 @@
+package ttv.poltoraha.pivka.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "notification")
+@Getter
+@Setter
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String text;
+}
