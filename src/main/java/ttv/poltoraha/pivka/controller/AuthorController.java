349c2666d6f3176b3bac9407c650bedf8ae2e9a3
Ilya Vehov <vehov.3@gmail.com>
2025-05-22 00:14:26 +0400

    chore: add logging for AuthorController and AuthorServiceImpl

--- src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package ttv.poltoraha.pivka.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
@@ -13,21 +14,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/author")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorController {
     private final AuthorService authorService;
 
     @PostMapping("/create")
     public void createAuthor(@RequestBody Author author) {
+        log.info("Received request to create author: {}", author);
         authorService.create(author);
+        log.info("Completed request to create author: {}", author);
     }
 
     @PostMapping("/delete")
     public void deleteAuthorById(@RequestParam Integer id) {
+        log.info("Received request to delete author: {}", id);
         authorService.delete(id);
+        log.info("Completed request to delete author: {}", id);
     }
 
     @PostMapping("/add/books")
     public void addBooksToAuthor(@RequestParam Integer id, @RequestBody List<Book> books) {
+        log.info("Received request to add books {} to author: {}", books, id);
         authorService.addBooks(id, books);
+        log.info("Completed request to add books {} to author: {}", books, id);
     }
 }
--- src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/ttv/poltoraha/pivka/serviceImpl/AuthorServiceImpl.java
@@ -1,6 +1,8 @@
 package ttv.poltoraha.pivka.serviceImpl;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,24 +18,26 @@ import java.util.List;
 // Имплементации интерфейсов с бизнес-логикой
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
 
-    // todo как будто надо насрать всякими мапперами
     @Override
-    @Transactional
     public void create(Author author) {
+        log.info("Request to author repository to create new author {}", author);
         authorRepository.save(author);
+        log.info("Completed request to author repository to create new author {}", author);
     }
 
     @Override
-    @Transactional
     public void delete(Integer id) {
+        log.info("Request to author repository to delete author by id {}", id);
         authorRepository.deleteById(id);
+        log.info("Completed request to author repository to delete author by id {}", id);
     }
 
     @Override
-    @Transactional
     public void addBooks(Integer id, List<Book> books) {
         val author = getOrThrow(id);
 
@@ -41,7 +45,6 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
-    @Transactional
     public void addBook(Integer id, Book book) {
         val author = getOrThrow(id);
 
@@ -49,21 +52,21 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
-    @Transactional
     public List<Author> getTopAuthorsByTag(String tag, int count) {
         Pageable pageable = PageRequest.of(0, count);
-        val authors = authorRepository.findTopAuthorsByTag(tag);
 
-        return authorRepository.findTopAuthorsByTag(tag, pageable);
+        log.info("Request to author repository to find top authors by tag {}", tag);
+        val authors = authorRepository.findTopAuthorsByTag(tag, pageable);
+        log.info("Completed request to author repository to find top authors by tag {}", tag);
+
+        return authors;
     }
 
     private Author getOrThrow(Integer id) {
-        val optionalAuthor = authorRepository.findById(id);
-        val author = optionalAuthor.orElse(null);
-
-        if (author == null) {
-            throw new RuntimeException("Author with id = " + id + " not found");
-        }
+        log.info("Request to author repository to find author by id {}", id);
+        val author = authorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Author with id = " + id + " not found"));
+        log.info("Completed request to author repository to find author by id {}", id);
 
         return author;
     }
