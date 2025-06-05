24b78dfcc074a58e14062b641c1f6f6f8b44db7d
Ilya Vehov <vehov.3@gmail.com>
2025-06-06 00:56:52 +0400

    feat: add able to find books by author top rating

--- src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
+++ src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
@@ -1,10 +1,18 @@
 package ttv.poltoraha.pivka.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ttv.poltoraha.pivka.entity.Book;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
-
+    @Query("SELECT b FROM book b JOIN b.author a WHERE a.id = (" +
+            "SELECT a2.id FROM author a2 WHERE a2.fullName LIKE CONCAT(:lastname, ' %') " +
+            "OR a2.fullName LIKE CONCAT('% ', :lastname) " +
+            "ORDER BY a2.avgRating DESC, a2.id ASC LIMIT 1)")
+    List<Book> findBooksByAuthorTopRating(@Param("lastname") String lastname);
 }
