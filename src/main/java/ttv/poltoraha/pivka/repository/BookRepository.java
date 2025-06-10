8bfe78d589b078f49ee43f0be2150d0c3bb1956b
Ilya Vehov <vehov.3@gmail.com>
2025-06-10 23:45:22 +0400

    feat: add recommendBook

--- src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
+++ src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package ttv.poltoraha.pivka.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ttv.poltoraha.pivka.entity.Book;
 
-@Repository
-public interface BookRepository extends CrudRepository<Book, Integer> {
+import java.util.List;
 
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTagsContaining(String tag);
 }
